004d19b026d0c863f914c2dc6dfd40722acde820
saif852 <160987694+saif852@users.noreply.github.com>
2025-06-06 11:23:42 -0600

    Update error.tf

--- error.tf
+++ error.tf
@@ -22,3 +22,8 @@
 "this":
 ¦  122: resource "aws_s3_bucket_notification" "this" {
 fatal: [sgzdavolbap100]: FAILED! => {"changed": true, "cmd": "\"F:\\\\IBM\\\\Installation Manager\\\\eclipse\\\\tools\\\\imcl.exe\" updateAll -iD \"F:\\\\IBM\\\\WebSphere\\\\AppServer\\\\\" -repositories \"F:\\\\temp\\\\patches_test\\\\repository.config\" -acceptLicense", "delta": "0:01:37.050163", "end": "2025-04-29 08:15:17.433527", "msg": "non-zero return code", "rc": 1, "start": "2025-04-29 08:13:40.383364", "stderr": "ERROR: Error during \"install\" phase:\r\n  ERROR: Deleting files from F:\\IBM\\WebSphere\\AppServer\r\n    ERROR: Failed to delete F:\\IBM\\WebSphere\\AppServer\\bin\\WASServiceMsg.dll\r\n", "stderr_lines": ["ERROR: Error during \"install\" phase:", "  ERROR: Deleting files from F:\\IBM\\WebSphere\\AppServer", "    ERROR: Failed to delete F:\\IBM\\WebSphere\\AppServer\\bin\\WASServiceMsg.dll"], "stdout": "", "stdout_lines": []}
+{
+  "code" : 400,
+  "message" : "Error with program: [File \"<program>\", line 7, in \n  filter=filter('aws_account_id', '471112763355')  
+      \r\nSyntaxError: invalid syntax],Program text is not a valid SignalFlow program: [File \"<program>\", line 7, in \n  filter=filter('aws_account_id', '471112763355')        \r\nSyntaxError: invalid syntax]"
+}
